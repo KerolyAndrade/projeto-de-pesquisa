611ec9cbf9dc3935f699dca4c682c6eb2ba53573
Keroly Andrade <84927887+KerolyAndrade@users.noreply.github.com>
2025-03-13 14:18:45 -0300

    Update CongregationsTableSeeder.php

--- database/seeders/CongregationsTableSeeder.php
+++ database/seeders/CongregationsTableSeeder.php
@@ -166,6 +166,55 @@ class CongregationsTableSeeder extends Seeder
     
         return $processedUrls; // Retornar URLs válidas como um array
     }
+    private function processStates($statesString)
+{
+    // Mapeamento de siglas para nomes completos
+    $stateMap = [
+        'AC' => 'Acre',
+        'AL' => 'Alagoas',
+        'AP' => 'Amapá',
+        'AM' => 'Amazonas',
+        'BA' => 'Bahia',
+        'CE' => 'Ceará',
+        'DF' => 'Distrito Federal',
+        'ES' => 'Espírito Santo',
+        'GO' => 'Goiás',
+        'MA' => 'Maranhão',
+        'MT' => 'Mato Grosso',
+        'MS' => 'Mato Grosso do Sul',
+        'MG' => 'Minas Gerais',
+        'PA' => 'Pará',
+        'PB' => 'Paraíba',
+        'PR' => 'Paraná',
+        'PE' => 'Pernambuco',
+        'PI' => 'Piauí',
+        'RJ' => 'Rio de Janeiro',
+        'RN' => 'Rio Grande do Norte',
+        'RS' => 'Rio Grande do Sul',
+        'RO' => 'Rondônia',
+        'RR' => 'Roraima',
+        'SC' => 'Santa Catarina',
+        'SP' => 'São Paulo',
+        'SE' => 'Sergipe',
+        'TO' => 'Tocantins',
+    ];
+
+    // Separar os estados da string
+    $statesArray = array_map('trim', explode(',', $statesString));
+
+    // Remover duplicatas e mapear para nomes completos
+    $uniqueStates = array_unique($statesArray);
+    $fullStateNames = [];
+
+    foreach ($uniqueStates as $state) {
+        $state = strtoupper(trim($state)); // Normaliza para maiúsculas
+        if (isset($stateMap[$state])) {
+            $fullStateNames[] = $stateMap[$state]; // Adiciona o nome completo
+        }
+    }
+
+    return $fullStateNames; // Retorna os nomes completos dos estados
+}
     
 
 
